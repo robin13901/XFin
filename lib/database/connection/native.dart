6f0412e8daebffe4ee4f8d223d9abb564fa5b805
I551358 <robin.pfaff@sap.com>
2025-10-30 13:08:43 +0100

    Optimized imports

--- lib/database/connection/native.dart
+++ lib/database/connection/native.dart
@@ -2,8 +2,8 @@ import 'dart:io';
 
 import 'package:drift/drift.dart';
 import 'package:drift_sqflite/drift_sqflite.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
 
 QueryExecutor connect() {
   return LazyDatabase(() async {
